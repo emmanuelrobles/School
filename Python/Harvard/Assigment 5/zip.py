d25eff875ba1533692f0399a0b40cd469579f872
emmanuelrobles <31362194+emmanuelrobles@users.noreply.github.com>
2017-10-17 09:54:00 -0400

    Update zip.py

--- Python/Harvard/Assigment 5/zip.py
+++ Python/Harvard/Assigment 5/zip.py
@@ -1,3 +1,9 @@
+"""
+The CVS file Missouri_Beer_Wine.cvsPreview the documentView in a new window lists suppliers to the state of Missouri.  Find the 5-digit zip code that holds the most suppliers.  When you see a 9-digit zip code, truncate the last 4 digits.  You can use Google to track down the winning zip code to validate your program.  
+
+Your program should print the number of suppliers in that zip code, and the zip code itself.  
+"""
+
 import collections
 import sys
 
